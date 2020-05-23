f5bff0ce503039511a050a101a7710eaab709ea6
Rebecca Fitzhugh <rmfitzhugh@gmail.com>
2020-05-23 13:33:36 -0700

    Update output.tf

--- 01-aws/output.tf
+++ 01-aws/output.tf
@@ -2,11 +2,11 @@
 #  Outputs required for later steps in automation workflow                     #
 ## =============================================================================
 # Specify this in CloudOut configuration for Rubrik cluster to use KMS for data encryption
-output "nova-kms" {
-  value = "${aws_kms_key.nova-kms.id}"
+output "mabel-kms" {
+  value = "${aws_kms_key.mabel-kms.id}"
 }
 
 # Specify this in CloudOn configuration for Rubrik cluster to use Bolt specific security group
-output "nova-bolt-sg-use1-c" {
-  value = "${aws_security_group.nova-bolt-sg-use1-c.id}"
+output "mabel-bolt-sg-use1-c" {
+  value = "${aws_security_group.mabel-bolt-sg-use1-c.id}"
 }
