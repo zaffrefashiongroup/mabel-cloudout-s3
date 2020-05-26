e0025e780920c1f600fa022ea76ee6553780b174
Rebecca Fitzhugh <rmfitzhugh@gmail.com>
2020-05-25 19:08:23 -0700

    Update main.tf

--- 03-rubrik-cloudon/main.tf
+++ 03-rubrik-cloudon/main.tf
@@ -3,7 +3,7 @@
 ## =============================================================================
 resource "rubrik_aws_s3_cloudon" "mabel-s3-use1-c" {
   archive_name      = var.rubrik_archive_name
-  vpc_id            = var.vpc_id
-  subnet_id         = var.subnet_id
-  security_group_id = var.security_group_id
+  vpc_id            = var.aws_vpc_id
+  subnet_id         = var.aws_subnet_id
+  security_group_id = var.aws_security_group_id
 }
